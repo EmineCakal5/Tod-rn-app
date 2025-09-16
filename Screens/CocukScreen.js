dfbaacad6acbba3780c8034fa1d754174d043691
EmineCakal5 <eminecakal668@gmail.com>
2025-09-16 15:26:28 +0300

    digiturk

--- Screens/CocukScreen.js
+++ Screens/CocukScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from 'react';
 import { Text, View, ScrollView, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import { ChevronRight, Tv } from 'lucide-react-native';
+import axios from "axios";
 
 export default function CocukScreen({ navigation }) {
 
@@ -9,17 +10,16 @@ export default function CocukScreen({ navigation }) {
 
   useEffect(() => {
     const fetchChildrens = async () => {
-      try {
-        const childrenRes = await fetch
-          ('https://api.themoviedb.org/3/discover/tv?api_key=ec55e243a5f3dc4d8ba5a4bedbfb52bd&with_genres=10762&language=tr-TR');
-        const childrenData = await childrenRes.json();
-        setChildrens(childrenData.results);
-      } catch (error) {
-        console.error(error);
-      }
+
+     axios.get('http://10.0.2.2:3333/todmena/boxsets').then((response) => {
+        setChildrens(response.data);  
+      }).catch((error) => {console.log(error)});
+
     }
     fetchChildrens();
   }, []);
+  
+
 
   return (
     <ScrollView style={{ backgroundColor: 'black' }}>
@@ -31,50 +31,49 @@ export default function CocukScreen({ navigation }) {
           horizontal={true}
           showsHorizontalScrollIndicator={false}
           style={{
-            position: 'absolute',
             zIndex: 10,
-           
+
           }}
           pagingEnabled
           snapToInterval={420}
-        > 
-          {childrens.slice(7, 14).map((childrens) => (
-            <View key={childrens.id} style={{ alignItems: "center" }}>
+        >
+          {childrens?.data?.map((childrens) => {
+            return (  <View key={childrens.id} style={{ alignItems: "center" }}>
               <TouchableOpacity
                 key={childrens.id}
                 onPress={() => navigation.navigate('DiziScreen', { childrensId: childrens.id })}
               >
                 <Image
-                  source={{ uri: `https://image.tmdb.org/t/p/w500${childrens.poster_path}` }}
+                  source={{ uri: childrens.posterUrl }}
                   style={{ width: 420, height: 350 }}
                   resizeMode='stretch'
                 />
               </TouchableOpacity>
-            </View>
-          ))}
+            </View>)
+          })}
         </ScrollView>
 
-          <View style={{
-                                        flexDirection: 'row',
-                                        justifyContent: 'space-between',
-                                        marginTop: -31,
-                            
-                                      }}
-                                      >
-                                        <TouchableOpacity
-                                          onPress={() => navigation.navigate('FormulaScreen')}>
-                                          <Text style={{ color: 'white', fontWeight: 'bold', marginTop: 70, fontSize: 16 }}>Öne Çıkanlar</Text>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity
-                                          onPress={() => navigation.navigate('FormulaScreen')}
-                                          style={{ marginTop: 70, borderWidth: 1, backgroundColor: '#1f1d17', borderRadius: 17, padding: 4 }} >
-                                          <ChevronRight
-                                            size={22}
-                                            color="gray"
-                                            fontWeight='bold'
-                                          />
-                                        </TouchableOpacity>
-                                      </View>
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginTop: -31,
+
+        }}
+        >
+          <TouchableOpacity
+            onPress={() => navigation.navigate('FormulaScreen')}>
+            <Text style={{ color: 'white', fontWeight: 'bold', marginTop: 70, fontSize: 16 }}>Öne Çıkanlar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('FormulaScreen')}
+            style={{ marginTop: 70, borderWidth: 1, backgroundColor: '#1f1d17', borderRadius: 17, padding: 4 }} >
+            <ChevronRight
+              size={22}
+              color="gray"
+              fontWeight='bold'
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
